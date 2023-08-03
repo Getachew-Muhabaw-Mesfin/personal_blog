364b129bdc25e49fb576b036c8169ef7582416fc
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2023-08-03 15:26:53 +0300

    Add base Html and 404 standard html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import Http404,HttpResponse
 
 # Create your views here.
+
+def starting_page(request):
+    try:
+      return render(request,'blog/index.html')
+    except:
+        raise Http404()
+    
+def posts(request):
+    pass
+
+def post_detial(request):
+    
+    pass
